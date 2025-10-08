5e270e085ca8781ce133b547c3de17e6e7387d55
Milen Krastev <sewerel@gmail.com>
2025-10-08 13:24:27 +0300

    Refactor CSS styles for design notes and update JavaScript file formatting
    - Updated CSS styles in style.min.css for design notes functionality, improving readability and organization.
    - Enhanced JavaScript code in ready.js by standardizing formatting, including consistent spacing and indentation.
    - Adjusted the archive glob patterns to improve clarity and maintainability.

--- ready.js
+++ ready.js
@@ -3,8 +3,8 @@ const path = require('path');
 const archiver = require('archiver');
 const dirName = path.basename(__dirname);
 let version = 'x.x.x';
-(function(){
-  const data = fs.readFileSync(dirName + '.php','utf8');
+(function () {
+  const data = fs.readFileSync(dirName + '.php', 'utf8');
   const pattern = /Version:\s*(?<version>[\d\.]+)/;
   const result = pattern.exec(data);
   version = result.groups.version;
@@ -19,14 +19,14 @@ const archive = archiver('zip');
 
 // listen for all archive data to be written
 //'close' event is fired only when a file descriptor is involved
-output.on('close', function() {
+output.on('close', function () {
   console.log(archive.pointer() + ' total bytes');
   console.log(`File created: D:/plugins/${dirName}-v.${version}.zip`);
 });
 
 
 // good practice to catch warnings (ie stat failures and other non-blocking errors)
-archive.on('warning', function(err) {
+archive.on('warning', function (err) {
   if (err.code === 'ENOENT') {
     // log warning
   } else {
@@ -36,33 +36,33 @@ archive.on('warning', function(err) {
 });
 archive.glob(
   //pattern match everything 
-  '**', 
+  '**',
   {
-  //where to look for
-   cwd: __dirname,
-   ignore: [
-    //ignore node_modules dir
-    'node_modules/**',
-    //Project specific
-    'assets/js/node_modules/**',
-    'assets/js/src/**',
-    'assets/js/*.mjml',
-    'assets/js/*.json',
-    'assets/js/*.map',
-    'assets/js/ready.js',
-    //ignore filename.ext 
-    '**ready.js', 
-    //ignore json extension all over
-    '*.json']
-    
-},
-{
-  //add parent directory name
-  prefix: dirName
-});
+    //where to look for
+    cwd: __dirname,
+    ignore: [
+      //ignore node_modules dir
+      'node_modules/**',
+      //Project specific
+      'assets/js/node_modules/**',
+      'assets/js/src/**',
+      'assets/js/*.mjml',
+      'assets/js/*.json',
+      'assets/js/*.map',
+      'assets/js/ready.js',
+      //ignore filename.ext 
+      '**ready.js',
+      //ignore json extension all over
+      '*.json']
+
+  },
+  {
+    //add parent directory name
+    prefix: dirName
+  });
 
 // good practice to catch this error explicitly
-archive.on('error', function(err) {
+archive.on('error', function (err) {
   throw err;
 });
 
